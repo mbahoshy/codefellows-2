2e16b786f5e80286cc40aa4576ae9788e526809e
mbahoshy <mbahoshy@gmail.com>
2013-09-27 11:36:13 -0700

    ready to deploy

--- js/functions.js
+++ js/functions.js
@@ -1,16 +1,23 @@
 function addTodo () {
+	//assigns input text to variable
 	x = $('input').val();
 	if (x) {
+		//if input text exists, appends todo to div
 		$('#todo_container').append('<div class="todo">' + x + '<img class="delete" src="delete.png"	/></div>');
+	} else {
+		alert('Please input text');
 	}
+	//clear input field
 	$('input').val("");
 }
 
+//deletes todo
 function deleteTodo () {
 	$(this).parent().remove();
 }
 
 $(document).ready(function(){
+	//assign click events
 	$('#todo_container').on('click', '.delete', deleteTodo);
 	$('#add_button').click(addTodo);
 });
